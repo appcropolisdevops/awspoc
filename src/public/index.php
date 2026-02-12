52f284e3be94a1d248d28440b80624cd59e5b22a
Naeem Dosh <m.naeem.uet.cs@gmail.com>
2026-02-13 02:31:06 +0500

    Update page title

--- src/public/index.php
+++ src/public/index.php
@@ -17,7 +17,7 @@ if (Auth::isLoggedIn()) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HIPAA POC - Secure Messaging</title>
+    <title>HIPAA POC - Secure Messaging Platform</title>
     <link rel="stylesheet" href="/assets/style.css">
 </head>
 <body>
